b303ea262ae8ffde1ca71ecddba2146902a4e64e
jameschapman19 <56592797+jameschapman19@users.noreply.github.com>
2020-01-06 16:14:40 +0000

    Add files via upload

--- Bee.py
+++ Bee.py
@@ -15,10 +15,10 @@ for word in wordlist:
     if wordnumber % 1000 == 0:
         print(wordnumber)
 
-#generate a tabular version of each word (number of words x 26 letters)
+# generate a tabular version of each word (number of words x 26 letters)
 df = pd.DataFrame.from_dict(counts)
 lettercounts = df.values
-#save the meaning of each column - the order gets confused
+# save the meaning of each column - the order gets confused
 letter_index = df.index.values
 lettercounts = np.transpose(lettercounts)
 lettercounts[np.isnan(lettercounts)] = 0
@@ -27,21 +27,22 @@ lettercounts[np.isnan(lettercounts)] = 0
 s = lettercounts[:, 7]
 lettercounts = lettercounts[s == 0]
 
-#filter for having no more than 7 unique letters
+# filter for having no more than 7 unique letters
 seven_max = np.count_nonzero(lettercounts, axis=1)
 lettercounts = lettercounts[seven_max < 8]
 
 # calculate points for each word
 score = np.sum(lettercounts, axis=1)
-#if pangram get 7 bonus
+# if pangram get 7 bonus
 score[np.count_nonzero(lettercounts, axis=1) == 7] += 7
+score[score == 4] = 1
 # expand to give score conditional on that letter being in centre of bee game (26 dimensional)
 score = (lettercounts > 0) * score[:, None]
 
 # filter pangram words
 pangrams = lettercounts[np.count_nonzero(lettercounts, axis=1) == 7]
 # extract the letters used in the pangrams
-letter_combinations = np.argwhere(pangrams > 0)[:,1].reshape(-1,7)
+letter_combinations = np.argwhere(pangrams > 0)[:, 1].reshape(-1, 7)
 
 # iterate through combinations
 combination_scores = np.zeros((len(letter_combinations), 7))
@@ -59,7 +60,7 @@ for letter_combination in letter_combinations:
 max_index = np.unravel_index(combination_scores.argmax(), combination_scores.shape)
 min_index = np.unravel_index(combination_scores.argmin(), combination_scores.shape)
 
-#print results
+# print results
 
 # print BEST combo
 best_letters = np.take(letter_index, letter_combinations[max_index[0]])
@@ -83,11 +84,14 @@ print("golden_letter = " + str(golden_letter))
 # print score
 print("score = " + str(combination_scores[min_index]))
 
-
 import matplotlib.pyplot as plt
+
 plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
 ax.hist(combination_scores)
+ax.set_xlabel('score')
+ax.set_ylabel('frequency')
+ax.set_title('Histogram of board scores')
 fig.tight_layout()
 plt.show()
 np.save('combination_scores.npy', combination_scores)
